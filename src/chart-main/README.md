1e53c50bebb83a9722ae40f52a6c9f658ebddf1f
tower1229 <tower1229@foxmail.com>
2022-04-18 20:51:17 +0800

    update

---
+++ src/chart-main/README.md
@@ -0,0 +1,43 @@
+# 柱形/线形图表
+
+### 介绍
+
+基于 Echart 封装的图表组件，支持线、柱两种类型
+
+### 引入
+
+```js
+import Vue from 'vue';
+import { ChartMain } from 'widgets';
+
+Vue.use(ChartMain);
+```
+
+## 代码演示
+
+### 基础用法
+
+```html
+<chart :data="data1" />
+```
+
+## API
+
+### Props
+
+| 参数          | 说明     | 类型     | 默认值    |
+| ------------- | -------- | -------- | --------- |
+| type          | 按钮类型 | _string_ | `primary` |
+| color `1.0.0` | 按钮颜色 | _string_ | -         |
+
+### Events
+
+| 事件名 | 说明       | 回调参数            |
+| ------ | ---------- | ------------------- |
+| click  | 点击时触发 | _event: MouseEvent_ |
+
+### Slots
+
+| 名称    | 说明     |
+| ------- | -------- |
+| default | 默认插槽 |
--- vant.config.js
+++ vant.config.js
@@ -12,10 +12,7 @@ module.exports = {
   site: {
     title: 'cutting-mat-widgets',
     logo: 'http://cutting-mat.refined-x.com/logo.png',
-    hideSimulator: true,
-    simulator: {
-      url: "/demo.html"
-    },
+    hideSimulator: false,
     nav: [
       {
         title: '开发指南',
@@ -35,6 +32,60 @@ module.exports = {
         items: [
           {
             path: 'demo-button',
+            title: 'DemoButton 按钮',
+          },
+        ],
+      },
+      {
+        title: '图表组件',
+        items: [
+          {
+            path: 'chart-main',
+            title: '柱形/线形图表',
+          },
+        ],
+      },
+      {
+        title: '业务组件',
+        items: [
+          {
+            path: 'business-button',
+            title: 'DemoButton 按钮',
+          },
+        ],
+      },
+      {
+        title: '布局组件',
+        items: [
+          {
+            path: 'layout-button',
+            title: 'DemoButton 按钮',
+          },
+        ],
+      },
+      {
+        title: '区块组件',
+        items: [
+          {
+            path: 'block-button',
+            title: 'DemoButton 按钮',
+          },
+        ],
+      },
+      {
+        title: '页面组件',
+        items: [
+          {
+            path: 'page-button',
+            title: 'DemoButton 按钮',
+          },
+        ],
+      },
+      {
+        title: '模板组件',
+        items: [
+          {
+            path: 'template-button',
             title: 'DemoButton 按钮',
           },
         ],
